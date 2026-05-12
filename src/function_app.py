13be1fbae78d48ab9c7863e91316284d7ec6ba0f
GUILHERME MIRANDA SILVA DOS SANTOS <guilherme.miranda@univille.br>
2026-05-12 19:54:52 -0300

    azurefunccionsdba

--- src/function_app.py
+++ src/function_app.py
@@ -10,3 +10,64 @@ def timer_trigger_aula2(myTimer: func.TimerRequest) -> None:
         logging.info('The timer is past due!')
 
     logging.info('Python timer trigger function executed.')
+    
+@app.timer_trigger(schedule="0 * * * * *", arg_name="myTimer", run_on_startup=False,
+              use_monitor=False) 
+def extract_categoria_produtos(myTimer: func.TimerRequest) -> None:
+    logging.info('tabela categoria produto')
+
+@app.timer_trigger(schedule="0 * * * * *", arg_name="myTimer", run_on_startup=False,
+              use_monitor=False) 
+def extract_cliente(myTimer: func.TimerRequest) -> None:
+    logging.info('tabela cliente')
+    
+@app.timer_trigger(schedule="0 * * * * *", arg_name="myTimer", run_on_startup=False,
+              use_monitor=False) 
+def extract_entrega(myTimer: func.TimerRequest) -> None:
+    logging.info('tabela entrega')    
+    
+@app.timer_trigger(schedule="0 * * * * *", arg_name="myTimer", run_on_startup=False,
+              use_monitor=False) 
+def extract_estoque_movimentacao(myTimer: func.TimerRequest) -> None:
+    logging.info('tabela estoque movimentacao')
+    
+@app.timer_trigger(schedule="0 * * * * *", arg_name="myTimer", run_on_startup=False,
+              use_monitor=False) 
+def extract_estoque_saldo(myTimer: func.TimerRequest) -> None:
+    logging.info('tabela estoque saldo')
+    
+@app.timer_trigger(schedule="0 * * * * *", arg_name="myTimer", run_on_startup=False,
+              use_monitor=False) 
+def extract_pedido(myTimer: func.TimerRequest) -> None:
+    logging.info('tabela pedidos')
+    
+@app.timer_trigger(schedule="0 * * * * *", arg_name="myTimer", run_on_startup=False,
+              use_monitor=False) 
+def extract_pedido_item(myTimer: func.TimerRequest) -> None:
+    logging.info('tabela pedido item')
+    
+@app.timer_trigger(schedule="0 * * * * *", arg_name="myTimer", run_on_startup=False,
+              use_monitor=False) 
+def extract_produto(myTimer: func.TimerRequest) -> None:
+    logging.info('tabela produto')
+    
+@app.timer_trigger(schedule="0 * * * * *", arg_name="myTimer", run_on_startup=False,
+              use_monitor=False) 
+def extract_regiao(myTimer: func.TimerRequest) -> None:
+    logging.info('tabela regiao')
+    
+@app.timer_trigger(schedule="0 * * * * *", arg_name="myTimer", run_on_startup=False,
+              use_monitor=False) 
+def extract_representante(myTimer: func.TimerRequest) -> None:
+    logging.info('tabela representante')
+    
+@app.timer_trigger(schedule="0 * * * * *", arg_name="myTimer", run_on_startup=False,
+              use_monitor=False) 
+def extract_titulo_receber(myTimer: func.TimerRequest) -> None:
+    logging.info('tabela titulo receber')
+    
+@app.timer_trigger(schedule="0 * * * * *", arg_name="myTimer", run_on_startup=False,
+              use_monitor=False) 
+def extract_transportadora(myTimer: func.TimerRequest) -> None:
+    logging.info('tabela tansportadora')
+    
