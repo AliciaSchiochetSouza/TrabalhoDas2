0c7b3fdb7ecd43d5863bd61ba3c11cea43d6e9d8
GUILHERME MIRANDA SILVA DOS SANTOS <guilherme.miranda@univille.br>
2026-06-16 20:27:05 -0300

    conexão com o banco

--- src/triggers/extract_tabela_categoria_produto.py
+++ src/triggers/extract_tabela_categoria_produto.py
@@ -14,38 +14,68 @@ def extract_categoria_produtos(myTimer: func.TimerRequest) -> None:
     sql_database = os.getenv("SQL_DATABASE_SOURCE")
     sql_user = os.getenv("SQL_USER_SOURCE")
     sql_password = os.getenv("SQL_PASSWORD_SOURCE")
+    
+    sql_server_destino = os.getenv("SQA_SERVER_DESTINO")
+    sql_database_destino = os.getenv("SQL_DATABASE_DESTINO")
+    sql_user_destino = os.getenv("SQL_USER_DESTINO")
+    sql_password_destino = os.getenv("SQL_PASSWORD_DESTINO")
 
-    logging.info(f"""servidor: {sql_server},banco: {sql_database}, usuário: {sql_user}, senha: {sql_password}""")
-
-    # Configura a string de conexão para o banco de dados SQL Server
-    conn_str = (
-        "DRIVER={ODBC Driver 18 for SQL Server};"
-        f"SERVER={sql_server};"
-        f"DATABASE={sql_database};"
-        f"UID={sql_user};"
-        f"PWD={sql_password};"
-        "Encrypt=yes;"
-        "TrustServerCertificate=no;"
-        "Connection Timeout=30;"
-    )
-
-   
     try:
-        # Estabelece a conexão com o banco de dados usando pyodbc
-        with pyodbc.connect(conn_str) as conn:
-            # Cria um cursor para executar a consulta   
-            cursor = conn.cursor()
+    # ---------- EXTRAÇÃO ----------
+
+        conn_str_source = (
+            "DRIVER={ODBC Driver 18 for SQL Server};"
+            f"SERVER={sql_server};"
+            f"DATABASE={sql_database};"
+            f"UID={sql_user};"
+            f"PWD={sql_password};"
+            "Encrypt=yes;"
+            "TrustServerCertificate=no;"
+            "Connection Timeout=30;"
+        )
+
+        with pyodbc.connect(conn_str_source) as conn_source:
+            cursor_source = conn_source.cursor()
+
+            query = "SELECT * FROM item.chamado"
+
+            cursor_source.execute(query)
+
+            rows = cursor_source.fetchall()
+
+            columns = [description[0] for description in cursor_source.description]
             
-            query = "select top 5 * from erp.categoria_produto"
+        # Configura a string de conexão para o banco de dados SQL Server
+        conn_str = (
+            "DRIVER={ODBC Driver 18 for SQL Server};"
+            f"SERVER={sql_server_destino};"
+            f"DATABASE={sql_database_destino};"
+            f"UID={sql_user_destino};"
+            f"PWD={sql_password_destino};"
+            "Encrypt=yes;"
+            "TrustServerCertificate=no;"
+            "Connection Timeout=30;"
+        )
+        try:
+            # Estabelece a conexão com o banco de dados usando pyodbc
+            with pyodbc.connect(conn_str) as conn:
+                # Cria um cursor para executar a consulta   
+                cursor = conn.cursor()
+                
+                query = "select top 5 * from erp.categoria_produto"
 
-            # Executa a consulta SQL
-            cursor.execute(query)
+                # Executa a consulta SQL
+                cursor.execute(query)
 
-            # Busca todos os resultados da consulta
-            rows = cursor.fetchall()
+                # Busca todos os resultados da consulta
+                rows = cursor.fetchall()
 
-            logging.info(rows)           
+                logging.info(rows)           
 
+        except Exception as e:
+            logging.error(f"Erro ao ler extract.tabela_categoria_produto: {str(e)}")
+            raise
     except Exception as e:
         logging.error(f"Erro ao ler extract.tabela_categoria_produto: {str(e)}")
         raise
+    
